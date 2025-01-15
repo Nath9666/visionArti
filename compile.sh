8cd8adcd4466bbd0161eb555bfb1ec4d97bf36be
Nathan <93925029+Nath9666@users.noreply.github.com>
2025-01-15 13:06:18 +0100

    rapport

--- compile.sh
+++ compile.sh
@@ -1,5 +1,13 @@
-# Compiler le fichier main.cpp
-g++ ./LegoTracker/src/main.cpp -o ./bin/main
+# Chemins vers les répertoires d'OpenCV et RapidJSON
+OPENCV_INCLUDE_DIR="C:/Users/Nathan/Documents/Projet/visionArti/opencv/build/include"
+OPENCV_LIB_DIR="C:/Users/Nathan/Documents/Projet/visionArti/opencv/build/x64/vc14/lib"
+RAPIDJSON_INCLUDE_DIR="C:/Users/Nathan/Documents/Projet/visionArti/include"
+
+# Créer le répertoire bin s'il n'existe pas
+mkdir -p ./bin
+
+# Compiler tous les fichiers source dans le répertoire src avec les chemins d'OpenCV et RapidJSON
+g++ -I${OPENCV_INCLUDE_DIR} -I${RAPIDJSON_INCLUDE_DIR} -L${OPENCV_LIB_DIR} ./src/*.cpp -o ./bin/main -lopencv_world460
 
 # Exécuter le fichier compilé
 ./bin/main
