568132045469d69dfd4ccdad196e368a844a0584
Nathan <93925029+Nath9666@users.noreply.github.com>
2025-02-08 23:03:24 +0100

    preparation tp

--- Tp/EFREI_IRV_VAAS/TP_Mouvement2025/src/Compile.sh
+++ Tp/EFREI_IRV_VAAS/TP_Mouvement2025/src/Compile.sh
@@ -1,5 +1,10 @@
+# Créer le répertoire bin s'il n'existe pas
+mkdir -p ../bin
+
+# Compiler EdMotionDetect
 gcc -o ../bin/EdMotionDetect EdMotionDetect.c EdLibMotionDetect.c EdUtilities.c
 echo "EdMotionDetect compiled"
 
+# Compiler EdGabMotionDetect
 gcc -o ../bin/EdGabMotionDetect EdGabMotionDetect.c EdLibGabMotionDetect.c EdUtilities.c
 echo "EdGabMotionDetect compiled"
