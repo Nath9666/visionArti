b26d8b742a01183301ae2671bb26344606b1c443
Nathan <93925029+Nath9666@users.noreply.github.com>
2025-02-09 20:08:57 +0100

    finish tp2

--- Tp/EFREI_IRV_VAAS/TP_Mouvement2025/src/Compile.sh
+++ Tp/EFREI_IRV_VAAS/TP_Mouvement2025/src/Compile.sh
@@ -1,23 +1,37 @@
+# Créer le répertoire bin s'il n'existe pas
 mkdir -p ../bin
 
+# Compiler EdMotionDetect
 gcc -o ../bin/EdMotionDetect EdMotionDetect.c EdLibMotionDetect.c EdUtilities.c
 echo "EdMotionDetect compiled"
 
+# Compiler EdMotionDetectFond
 gcc -o ../bin/EdMotionDetectFond EdMotionDetectFond.c EdLibMotionDetectFond.c EdUtilities.c
 echo "EdMotionDetectFond compiled"
 
+# Créer le répertoire ImRes s'il n'existe pas
 mkdir -p ../ImRes
 echo "ImRes directory created"
 
 cd ../bin
 
+# Exécuter EdMotionDetect
 ./EdMotionDetect.exe NImCote0000.ppm 30
 
-# deplacer tout les fichiers dans ImRes dans le dossier NImCote
+# Créer le répertoire NImCote dans ImRes s'il n'existe pas
+mkdir -p ../ImRes/NImCote
 
-cd ../ImRes
+# Déplacer tous les fichiers générés dans ImRes vers le dossier NImCote
+mv ../ImRes/* ../ImRes/NImCote/
 
-
+# Exécuter EdMotionDetectFond
 ./EdMotionDetectFond.exe NImCote0000.ppm 30 0.01
 
+# Créer le répertoire NImCoteFond dans ImRes s'il n'existe pas
+mkdir -p ../ImRes/NImCoteFond
 
+# Déplacer tous les fichiers générés dans ImRes vers le dossier NImCoteFond
+mv ../ImRes/* ../ImRes/NImCoteFond/
+
+echo "EdMotionDetect and EdMotionDetectFond executed"
+mv ../ImRes/NImCoteFond/NImCote ../ImRes/
