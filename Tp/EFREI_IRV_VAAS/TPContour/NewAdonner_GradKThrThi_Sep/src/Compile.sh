877335fa4e4811c1417202f0061887c42fe867e4
Nathan <93925029+Nath9666@users.noreply.github.com>
2025-02-09 11:25:15 +0100

    all the photo

--- Tp/EFREI_IRV_VAAS/TPContour/NewAdonner_GradKThrThi_Sep/src/Compile.sh
+++ Tp/EFREI_IRV_VAAS/TPContour/NewAdonner_GradKThrThi_Sep/src/Compile.sh
@@ -4,14 +4,15 @@ mkdir -p ../bin
 # Compiler EdGradientKirsh
 gcc -o ../bin/EdGradientKirsh DetectionContours.c EdLibGradientKirsh4SepMasks.c EdLibThinning.c EdLibEdgeUtilities.c EdUtilities.c EdLibThreshold.c
 
+
 cd ../bin
 
 # Définir les variables d'image
-nom_image="Bureau"
+nom_image="PavinReal"
 
 image="../image/${nom_image}.pgm"
 imageNorm="../ImRes/${nom_image}_norm.pgm"
 imagePts="../ImRes/${nom_image}_pts.pgm"
 
 # Exécuter le programme avec les arguments appropriés
-./EdGradientKirsh "$image" "$imageNorm" "$imagePts" 10
+./EdGradientKirsh "$image" "$imageNorm" "$imagePts" 20
